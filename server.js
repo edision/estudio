8d2df07dd593dc5612ad39aa03040735ea867ef7
edision <boywujch@163.com>
2017-04-24 17:23:49 +0800

    server.js新增NODE_ENV判断

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const Koa = require("koa");
 const app = new Koa();
 
-
+const isDev = process.env.NODE_ENV === "development";
+if(isDev) {
+  // 加载webpack中间件及配置
+}
 
 app.listen(3000, function(){
   console.log("Web服务器已启动...");
-})
+});
