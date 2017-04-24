5cc3a8cf4cfab8872da772fbeafa4259b134227e
edision <boywujch@163.com>
2017-04-24 17:48:19 +0800

    开始搭建react开发环境

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const Koa = require("koa");
 const app = new Koa();
 
 const isDev = process.env.NODE_ENV === "development";
-if(isDev) {
-  // 加载webpack中间件及配置
+if (isDev) {
+    // 加载webpack中间件及配置
+    console.log("配置开发环境...");
+    const webpackMiddleware = require("koa-webpack");
+    const webpackConfig = require("./webpack.config");
+    const webpack = require("webpack");
+    app.use(webpackMiddleware({
+      compiler: webpack(webpackConfig)
+    }));
 }
 
-app.listen(3000, function(){
-  console.log("Web服务器已启动...");
+app.listen(3000, function() {
+    console.log("Web服务器已启动...");
 });
