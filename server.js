e3ca6f658eae0e7d5e99a186f1bd2533b83425cc
吴剑超 <boywujch@hotmail.com>
2017-05-22 23:18:47 +0800

    解决koa-webpack覆盖ctx.body

--- server.js
+++ server.js
@@ -2,38 +2,6 @@ import Koa from 'koa';
 import path from 'path';
 const app = new Koa();
 
-const isDev = process.env.NODE_ENV === "development";
-if (isDev) {
-    // 加载webpack中间件及配置
-    console.log("配置开发环境...");
-    const webpackMiddleware = require("koa-webpack");
-    const config = require("./webpack.config");
-    // const webpack = require("webpack");
-    app.use(webpackMiddleware({
-        config: config,
-        dev: {
-            publicPath: config.output.publicPath,
-            stats: {
-                colors: true
-            },
-            proxy:{
-                "/api":{
-                    target: "http://localhost:3000"
-                }
-            }
-        }
-    }));
-    console.log("配置完成");
-} else {
-    console.log("配置生产环境...");
-    // 生产环境中间件
-    const favicon = require("koa-favicon");
-    const serve = require("koa-static");
-    app.use(favicon(path.join(__dirname, "/public")));
-    app.use(serve(path.join(__dirname, "/public")));
-    console.log("配置完成");
-}
-
 import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/estudio");
@@ -65,6 +33,35 @@ app.use(async ctx => {
         console.error(error);
     }
 });
+
+const isDev = process.env.NODE_ENV === "development";
+if (isDev) {
+    // 加载webpack中间件及配置
+    console.log("配置开发环境...");
+    const webpackMiddleware = require("koa-webpack");
+    const config = require("./webpack.config");
+    // const webpack = require("webpack");
+    app.use(webpackMiddleware({
+        config: config,
+        dev: {
+            publicPath: config.output.publicPath,
+            stats: {
+                colors: true
+            }
+        }
+    }));
+    console.log("配置完成");
+} else {
+    console.log("配置生产环境...");
+    // 生产环境中间件
+    const favicon = require("koa-favicon");
+    const serve = require("koa-static");
+    app.use(favicon(path.join(__dirname, "/public")));
+    app.use(serve(path.join(__dirname, "/public")));
+    console.log("配置完成");
+}
+
+
 
 const port = 3000;
 app.listen(port, function() {
