c9edb72c5b4b35a02a4c55f487049cb16e9ebaa1
吴剑超 <boywujch@hotmail.com>
2017-05-23 22:22:33 +0800

    完成页面布局

--- server.js
+++ server.js
@@ -2,38 +2,6 @@ import Koa from 'koa';
 import path from 'path';
 const app = new Koa();
 
-import mongoose from 'mongoose';
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/estudio");
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('mongodb connected!');
-});
-
-var UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
-}, {
-    versionKey: false,
-    collection: 'User' // 自定义集合名称(表名)
-});
-var User = mongoose.model('User', UserSchema);
-
-app.use(async ctx => {
-    let test = new User({ name: 'hello first mongo' });
-    try {
-        let saved = await test.save();
-        console.log('saved', saved.name);
-        ctx.body = saved.name;
-    } catch (error) {
-        console.error(error);
-    }
-});
-
 const isDev = process.env.NODE_ENV === "development";
 if (isDev) {
     // 加载webpack中间件及配置
@@ -60,8 +28,6 @@ if (isDev) {
     app.use(serve(path.join(__dirname, "/public")));
     console.log("配置完成");
 }
-
-
 
 const port = 3000;
 app.listen(port, function() {
---
+++ src/components/complexparam/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class List extends React.Component {
+  render() {
+    return (
+      <h2>复合参数</h2>
+    );
+  }
+}
+
+export default List
