9da241aa134b91c5a97b93665a4401e8a25e1e9b
edision <boywujch@163.com>
2017-06-02 15:57:57 +0800

    完成unicode中文互转功能

--- src/components/tools/crypt/unicode.jsx
+++ src/components/tools/crypt/unicode.jsx
@@ -1,18 +1,51 @@
 import React from 'react';
-import { Input } from 'qnui';
+import { Input, Grid, Button } from 'qnui';
+const Row = Grid.Row;
+const Col = Grid.Col;
 
 class UnicodeChsConvert extends React.Component {
-  render() {
+  state = { inputValue: '', outputValue: '' };
 
+  handleSrcChange = (value, e) => this.setState({ inputValue: value });
+  handleDistChange = (value, e) => this.setState({ outputValue: value });
+  handleToChs = e => {
+    e.preventDefault();
+    let outputValue = unescape(this.state.inputValue.replace(/\\/g, '%'));
+    this.setState({outputValue: outputValue});
+  }
+
+  handleToUnicode = e => {
+    e.preventDefault();   
+    let outputValue = escape(this.state.inputValue).replace(/%/g, '\\');
+    this.setState({outputValue: outputValue});
+  }
+  render() {
+    const { inputValue, outputValue } = this.state;
     return (
       <div className="inner-container">
         <div>
           <h2>Unicode中文互转</h2>
-          <p>&emsp;实现Unicode与中文之间的相互转换功能，用户可根据需要输入中文或Unicode编码，点击下方对应按钮实现转换对应Unicode编码与中文的功能。</p>
-          <hr />
+          <p>1. 提供一个中文汉字Unicode互转、 ASCII与Unicode互转的在线工具，方便帮助你解决中文的乱码问题。</p>
+          <p>2. 也许你还需要：UrlEncode编码 / UrlDecode解码（gbk, big5, utf8）</p>         
+        </div>
+         <hr />
+        <div className="inner-wrapper">
+          <Row>
+            <Col span="12">
+              <Input multiple rows={10} style={{ width: '100%' }} placeholder="请输入待转换的中文或unicode编码..." value={inputValue} onChange={this.handleSrcChange} />
+            </Col>
+            <Col span="12">
+              <Input multiple rows={10} style={{ width: '100%' }} placeholder="转换后的文本，可直接用于html" value={outputValue} onChange={this.handleDistChange} />
+            </Col>
+          </Row>
         </div>
         <div className="inner-wrapper">
-          <Input multiple rows={10} />
+          <Row>
+            &emsp;
+            <Button onClick={this.handleToUnicode}>中文转Unicode</Button>
+            &emsp;
+            <Button onClick={this.handleToChs}>Unicode转中文</Button>
+          </Row>
         </div>
       </div>
     );
