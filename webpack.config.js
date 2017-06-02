70480ae77ea804298336b2d678a0328e615fdcfc
edision <boywujch@163.com>
2017-06-02 17:19:38 +0800

    拆分vender,bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require("webpack");
 var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 var debug = process.env.NODE_ENV === "development";
 
@@ -8,65 +9,67 @@ var debug = process.env.NODE_ENV === "development";
 var plugins = [new HtmlWebpackPlugin({
     inject: "body",
     template: "./src/index.html"
-})];
+}), new webpack.DefinePlugin({
+    DEBUG: debug
+}), new webpack.optimize.CommonsChunkPlugin({name: 'vender', filename:'vender.js'})];
 
 if (debug) {
-    plugins.push(new webpack.DefinePlugin({
-            DEBUG: true
-        }),
+    plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin()
     );
 } else {
     plugins.push(
         new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify('production')
-                }
-            }),
-            // new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.OccurrenceOrderPlugin(),
-            new webpack.optimize.UglifyJsPlugin({
-                mangle: false,
-                sourcemap: false
-            })
-        );
-    }
-
-    module.exports = {
-        entry: debug ? [
-            'babel-polyfill',
-            'react-hot-loader/patch',
-            'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-            './src/index.js'
-        ] : ['babel-polyfill', "./src/index.js"],
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "bundle.js?qtag=[hash:8]",
-            publicPath: "/"
-        },
-        module: {
-            rules: [
-            {
-                test: /\.jsx?$/,
-                include: [
-                    path.resolve(__dirname, "src")
-                ],
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            }]
-        },
-        resolve: {
-            extensions: [".js", ".jsx", ".css"],
-            alias: {
-                "COMPONENTS": path.resolve(__dirname, "src", "components"),
-                "STORES": path.resolve(__dirname, "src", "stores"),
-                "UTILS": path.resolve(__dirname, "src", "utils"),
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
             }
+        }),
+        // new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname, "dist"),
+            to: path.resolve(__dirname, "public")
+        }], {
+            copyUnmodified: true
+        })
+    );
+}
+
+const vender = ['babel-polyfill', 'react', 'react-dom', 'react-router-dom', 'mobx', 'mobx-react', 'mobx-utils', 'react-addons-transition-group', 'moment', 'qnui'];
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js',
+        vender: debug ? vender.push['react-hot-loader/patch', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000'] : vender
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js?qntag=[hash:8]",
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+        {
+            test: /\.jsx?$/,
+            include: [
+                path.resolve(__dirname, "src")
+            ],
+            loader: "babel-loader"
         },
-        devtool: debug ? "source-map" : false,
-        plugins: plugins
-    }
+        {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".css"],
+        alias: {
+            "COMPONENTS": path.resolve(__dirname, "src", "components"),
+            "STORES": path.resolve(__dirname, "src", "stores"),
+            "UTILS": path.resolve(__dirname, "src", "utils"),
+        }
+    },
+    devtool: debug ? "source-map" : false,
+    plugins: plugins
+}
